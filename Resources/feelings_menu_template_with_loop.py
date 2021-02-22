ef6c07f20e1cd27744e5c94e12f095ecf010b445
MBASTeacher <rossm@mbas.ac.nz>
2021-02-23 09:07:35 +1300

    Added a comment

--- Resources/feelings_menu_template_with_loop.py
+++ Resources/feelings_menu_template_with_loop.py
@@ -1,18 +1,19 @@
-# This is the feelings menu template for the resources.
+# This is the feelings menu exemplar.
 #
 # https://drive.google.com/drive/folders/1tD1FfW9UpQc-_9hjIiFpSaZiscEaCtbv
 #
 
 # Output a welcome message.
-print("Testing changes.")
-print("Add more of these to output more lines.")
-print("You can make fancy boxes with characters.")
+
 print()
-print("#############")
-print("#           #")
-print("# Like this #")
-print("#           #")
-print("#############")
+print("############################")
+print("#                          #")
+print("# QUOTES FOR YOUR FEELINGS #")
+print("#                          #")
+print("############################")
+print()
+print("Welcome. You tell me how you are feeling and I will give you a quote.")
+print()
 
 # We set this to True. As long as it is true we run the next loop.
 # To stop the loop we just set this to False.
@@ -22,17 +23,22 @@ run_feelings = True
 while run_feelings:
 # We could also use while run_feelings == True. while run_feelings is shorter!
 
-
+    print("You can choose from the following list of feelings. Which one best matches your mood right now?")
+    print()
     # Output a feeling
-    print("A: I Feel a bit worried.")
+    print("A: I feel really happy.")
+    print("B: I feel really sad.")
+    print("C: I feel a bit worried.")
+    print("D: I feel excited.")
     print("Q: To quit.")
+    print()
 
     # Output instructions
     print("Enter the letter that corresponds the most closely to how you feel.")
 
     # Creates a variable called feeling that stores the users input.
     # Notice the space. It's important.
-    feeling = input("Choose a feeling from the options (A): ")
+    feeling = input("Choose a feeling from the options (A, B, C, D) or Q to quit: ")
 
     # Process the user input.
     if feeling == "A":
@@ -41,6 +47,13 @@ while run_feelings:
         print()
         print("Janis Joplin")
         print()
+        if feeling == "A":
+            # This is for worry
+            print()
+            print("You can destroy your now, by worrying about tomorrow.")
+            print()
+            print("Janis Joplin")
+            print()
     elif feeling == "Q":
         print("OK, it was fun while it lasted.")
 
