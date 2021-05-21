4eef16db307d5fc7263af41f2511b14765e6d7a0
MBASTeacher <rossm@mbas.ac.nz>
2021-05-21 15:03:46 +1200

    Fixed width stretching. Thanks Nathan. Added shooting with mouse. Made bullets hit coins and deleted bullets that leave the map

--- GameDevelopment/game_file.py
+++ GameDevelopment/game_file.py
@@ -4,11 +4,12 @@ Platformer Game
 python -m arcade.examples.platform_tutorial.11_animate_character
 """
 import arcade
+import math
 import os
 
 # Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Platformer"
 
 # Constants used to scale our sprites from their original size
@@ -16,6 +17,7 @@ TILE_SCALING = 0.5
 CHARACTER_SCALING = TILE_SCALING * 2
 COIN_SCALING = TILE_SCALING
 SPRITE_PIXEL_SIZE = 128
+SPRITE_SCALING_LASER = 0.8
 GRID_PIXEL_SIZE = (SPRITE_PIXEL_SIZE * TILE_SCALING)
 
 # Movement speed of player, in pixels per frame
@@ -36,6 +38,8 @@ PLAYER_START_Y = SPRITE_PIXEL_SIZE * TILE_SCALING * 1
 # Constants used to track if the player is facing left or right
 RIGHT_FACING = 0
 LEFT_FACING = 1
+
+BULLET_SPEED = 5
 
 
 def load_texture_pair(filename):
@@ -143,6 +147,7 @@ class PlayerCharacter(arcade.Sprite):
             self.cur_texture = 0
         self.texture = self.walk_textures[self.cur_texture][self.character_face_direction]
 
+
 class InstructionView(arcade.View):
     """
     Shows the instructions for the game.
@@ -205,6 +210,7 @@ class GameOverView(arcade.View):
         game_view.setup()
         self.window.show_view(game_view)
 
+
 class GameView(arcade.View):
     """
     Main application class.
@@ -216,7 +222,7 @@ class GameView(arcade.View):
         """
 
         # Call the parent class and set up the window
-        #super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        # super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
         # Changed for a view.
         super().__init__()
 
@@ -238,6 +244,7 @@ class GameView(arcade.View):
         self.background_list = None
         self.ladder_list = None
         self.player_list = None
+        self.bullet_list = None
 
         # Separate variable that holds the player sprite
         self.player_sprite = None
@@ -258,6 +265,7 @@ class GameView(arcade.View):
         self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
         self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
         self.game_over = arcade.load_sound(":resources:sounds/gameover1.wav")
+        self.bullet_sound = arcade.load_sound(":resources:sounds/laser1.wav")
 
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
@@ -274,6 +282,7 @@ class GameView(arcade.View):
         self.background_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
 
         # Set up the player, specifically placing it at these coordinates.
         self.player_sprite = PlayerCharacter()
@@ -347,6 +356,7 @@ class GameView(arcade.View):
         self.ladder_list.draw()
         self.coin_list.draw()
         self.player_list.draw()
+        self.bullet_list.draw()
 
         # Draw our score on the screen, scrolling it with the viewport
         score_text = f"Score: {self.score}"
@@ -427,6 +437,44 @@ class GameView(arcade.View):
 
         self.process_keychange()
 
+    def on_mouse_press(self, x, y, button, modifiers):
+        """ Called whenever the mouse button is clicked. """
+
+        # Create a bullet
+        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
+
+        # Position the bullet at the player's current location
+        start_x = self.player_sprite.center_x
+        start_y = self.player_sprite.center_y
+        bullet.center_x = start_x
+        bullet.center_y = start_y
+
+        # Get from the mouse the destination location for the bullet
+        # IMPORTANT! If you have a scrolling screen, you will also need
+        # to add in self.view_bottom and self.view_left.
+        dest_x = x + self.view_left
+        dest_y = y + self.view_bottom
+
+        # Do math to calculate how to get the bullet to the destination.
+        # Calculation the angle in radians between the start points
+        # and end points. This is the angle the bullet will travel.
+        x_diff = dest_x - start_x
+        y_diff = dest_y - start_y
+        angle = math.atan2(y_diff, x_diff)
+
+        # Angle the bullet sprite so it doesn't look like it is flying
+        # sideways.
+        bullet.angle = math.degrees(angle)
+        # DEBUG # print(f"Bullet angle: {bullet.angle:.2f}")
+
+        # Taking into account the angle, calculate our change_x
+        # and change_y. Velocity is how fast the bullet travels.
+        bullet.change_x = math.cos(angle) * BULLET_SPEED
+        bullet.change_y = math.sin(angle) * BULLET_SPEED
+
+        # Add the bullet to the appropriate lists
+        self.bullet_list.append(bullet)
+
     def on_update(self, delta_time):
         """ Movement and game logic """
 
@@ -452,6 +500,41 @@ class GameView(arcade.View):
 
         # Update walls, used with moving platforms
         self.wall_list.update()
+
+        # Update bullets.
+        self.bullet_list.update()
+
+        # Loop through each bullet
+        for bullet in self.bullet_list:
+
+            # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(bullet, self.coin_list)
+
+            # If it did, get rid of the bullet
+            if len(hit_list) > 0:
+                bullet.remove_from_sprite_lists()
+
+            # For every coin we hit, add to the score and remove the coin
+            for coin in hit_list:
+                coin.remove_from_sprite_lists()
+                self.score += 1
+                arcade.play_sound(self.collect_coin_sound)
+
+            # If the bullet flies off-screen, remove it.
+            if bullet.bottom < self.view_bottom \
+                    or bullet.top > SCREEN_HEIGHT + self.view_bottom \
+                    or bullet.right > self.view_left + SCREEN_WIDTH \
+                    or bullet.left < self.view_left:
+                bullet.remove_from_sprite_lists()
+                print("A bullet left the view and is deleted.", bullet.bottom, self.view_bottom )
+
+            # If the bullet hits a platform, remove it.
+            # Check this bullet to see if it hit a wall
+            hit_list = arcade.check_for_collision_with_list(bullet, self.wall_list)
+
+            # If it did, get rid of the bullet
+            if len(hit_list) > 0:
+                bullet.remove_from_sprite_lists()
 
         # See if the moving wall hit a boundary and needs to reverse direction.
         for wall in self.wall_list:
