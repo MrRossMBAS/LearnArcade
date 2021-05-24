bab0bea675f71045ba244c6f9fb820685f6c7c06
MBASTeacher <rossm@mbas.ac.nz>
2021-05-25 11:32:36 +1200

    Made coins explode. Using particles and smoke. Also played starting sound to remove first sound lag.

--- GameDevelopment/game_file.py
+++ GameDevelopment/game_file.py
@@ -5,6 +5,7 @@ python -m arcade.examples.platform_tutorial.11_animate_character
 """
 import arcade
 import math
+import random
 import os
 
 # Constants
@@ -40,6 +41,125 @@ RIGHT_FACING = 0
 LEFT_FACING = 1
 
 BULLET_SPEED = 5
+
+# --- Explosion Particles Related
+
+# How fast the particle will accelerate down. Make 0 if not desired
+PARTICLE_GRAVITY = 0.05
+
+# How fast to fade the particle
+PARTICLE_FADE_RATE = 8
+
+# How fast the particle moves. Range is from 2.5 <--> 5 with 2.5 and 2.5 set.
+PARTICLE_MIN_SPEED = 2.5
+PARTICLE_SPEED_RANGE = 2.5
+
+# How many particles per explosion
+PARTICLE_COUNT = 20
+
+# How big the particle
+PARTICLE_RADIUS = 3
+
+# Possible particle colors
+PARTICLE_COLORS = [arcade.color.ALIZARIN_CRIMSON,
+                   arcade.color.COQUELICOT,
+                   arcade.color.LAVA,
+                   arcade.color.KU_CRIMSON,
+                   arcade.color.DARK_TANGERINE]
+
+# Chance we'll flip the texture to white and make it 'sparkle'
+PARTICLE_SPARKLE_CHANCE = 0.02
+
+# --- Smoke
+# Note: Adding smoke trails makes for a lot of sprites and can slow things
+# down. If you want a lot, it will be necessary to move processing to GPU
+# using transform feedback. If to slow, just get rid of smoke.
+
+# Start scale of smoke, and how fast is scales up
+SMOKE_START_SCALE = 0.25
+SMOKE_EXPANSION_RATE = 0.03
+
+# Rate smoke fades, and rises
+SMOKE_FADE_RATE = 7
+SMOKE_RISE_RATE = 0.5
+
+# Chance we leave smoke trail
+SMOKE_CHANCE = 0.25
+
+
+class Smoke(arcade.SpriteCircle):
+    """ This represents a puff of smoke """
+    def __init__(self, size):
+        super().__init__(size, arcade.color.LIGHT_GRAY, soft=True)
+        self.change_y = SMOKE_RISE_RATE
+        self.scale = SMOKE_START_SCALE
+
+    def update(self):
+        """ Update this particle """
+        if self.alpha <= PARTICLE_FADE_RATE:
+            # Remove faded out particles
+            self.remove_from_sprite_lists()
+        else:
+            # Update values
+            self.alpha -= SMOKE_FADE_RATE
+            self.center_x += self.change_x
+            self.center_y += self.change_y
+            self.scale += SMOKE_EXPANSION_RATE
+
+
+class Particle(arcade.SpriteCircle):
+    """ Explosion particle """
+    def __init__(self, my_list):
+        # Choose a random color
+        color = random.choice(PARTICLE_COLORS)
+
+        # Make the particle
+        super().__init__(PARTICLE_RADIUS, color)
+
+        # Track normal particle texture, so we can 'flip' when we sparkle.
+        self.normal_texture = self.texture
+
+        # Keep track of the list we are in, so we can add a smoke trail
+        self.my_list = my_list
+
+        # Set direction/speed
+        speed = random.random() * PARTICLE_SPEED_RANGE + PARTICLE_MIN_SPEED
+        direction = random.randrange(360)
+        self.change_x = math.sin(math.radians(direction)) * speed
+        self.change_y = math.cos(math.radians(direction)) * speed
+
+        # Track original alpha. Used as part of 'sparkle' where we temp set the
+        # alpha back to 255
+        self.my_alpha = 255
+
+        # What list do we add smoke particles to?
+        self.my_list = my_list
+
+    def update(self):
+        """ Update the particle """
+        if self.my_alpha <= PARTICLE_FADE_RATE:
+            # Faded out, remove
+            self.remove_from_sprite_lists()
+        else:
+            # Update
+            self.my_alpha -= PARTICLE_FADE_RATE
+            self.alpha = self.my_alpha
+            self.center_x += self.change_x
+            self.center_y += self.change_y
+            self.change_y -= PARTICLE_GRAVITY
+
+            # Should we sparkle this?
+            if random.random() <= PARTICLE_SPARKLE_CHANCE:
+                self.alpha = 255
+                self.texture = arcade.make_circle_texture(self.width, arcade.color.WHITE)
+            else:
+                self.texture = self.normal_texture
+
+            # Leave a smoke particle?
+            if random.random() <= SMOKE_CHANCE:
+                smoke = Smoke(5)
+                smoke.position = self.position
+                self.my_list.append(smoke)
 
 
 def load_texture_pair(filename):
@@ -318,6 +438,7 @@ class GameView(arcade.View):
         self.ladder_list = None
         self.player_list = None
         self.bullet_list = None
+        self.explosions_list = None
 
         # Separate variable that holds the player sprite
         self.player_sprite = None
@@ -357,6 +478,7 @@ class GameView(arcade.View):
         self.wall_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
+        self.explosions_list = arcade.SpriteList()
 
         # Set up the player, specifically placing it at these coordinates.
         self.player_sprite = PlayerCharacter()
@@ -423,6 +545,9 @@ class GameView(arcade.View):
                                                              gravity_constant=GRAVITY,
                                                              ladders=self.ladder_list)
 
+        # Play a sound to try and remove first sound pausing.
+        arcade.play_sound(self.collect_coin_sound)
+
     def return_from_pause(self):
         """Reset anything we changed during the pause."""
 
@@ -443,6 +568,7 @@ class GameView(arcade.View):
         self.coin_list.draw()
         self.player_list.draw()
         self.bullet_list.draw()
+        self.explosions_list.draw()
 
         # Draw our score on the screen, scrolling it with the viewport
         score_text = f"Score: {self.score}"
@@ -595,6 +721,9 @@ class GameView(arcade.View):
         # Update bullets.
         self.bullet_list.update()
 
+        # Update explosions
+        self.explosions_list.update()
+
         # Loop through each bullet
         for bullet in self.bullet_list:
 
@@ -607,6 +736,16 @@ class GameView(arcade.View):
 
             # For every coin we hit, add to the score and remove the coin
             for coin in hit_list:
+                # Make an explosion
+                for i in range(PARTICLE_COUNT):
+                    particle = Particle(self.explosions_list)
+                    particle.position = coin.position
+                    self.explosions_list.append(particle)
+
+                smoke = Smoke(50)
+                smoke.position = coin.position
+                self.explosions_list.append(smoke)
+
                 coin.remove_from_sprite_lists()
                 self.score += 1
                 arcade.play_sound(self.collect_coin_sound)
@@ -617,7 +756,6 @@ class GameView(arcade.View):
                     or bullet.right > self.view_left + SCREEN_WIDTH \
                     or bullet.left < self.view_left:
                 bullet.remove_from_sprite_lists()
-                print("A bullet left the view and is deleted.", bullet.bottom, self.view_bottom)
 
             # If the bullet hits a platform, remove it.
             # Check this bullet to see if it hit a wall
