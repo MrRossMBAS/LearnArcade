e9ff80964005b96d026ba346190ed07104e1d425
MBASTeacher <rossm@mbas.ac.nz>
2021-05-26 11:28:38 +1200

    Added ammo and max bullets in the air.

--- GameDevelopment/game_file.py
+++ GameDevelopment/game_file.py
@@ -91,7 +91,7 @@ SMOKE_CHANCE = 0.25
 # The player can only shoot so many bullets at once.
 # Currently these are hard coded here, but they will be updatable later.
 AMMO = 4
-MAX_BULLETS = 4
+MAX_BULLETS = 1
 
 class Smoke(arcade.SpriteCircle):
     """ This represents a puff of smoke """
@@ -664,43 +664,46 @@ class GameView(arcade.View):
     def on_mouse_press(self, x, y, button, modifiers):
         """ Called whenever the mouse button is clicked. """
 
-        # Create a bullet
-        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
+        # The player can only shoot (we create a bullet)
+        # if there is available ammo and the maximum number of bullets don't already exist.
+        if AMMO > 0 and len( self.bullet_list) < MAX_BULLETS:
+            # Create a bullet
+            bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
 
-        # Position the bullet at the player's current location
-        start_x = self.player_sprite.center_x
-        start_y = self.player_sprite.center_y
-        bullet.center_x = start_x
-        bullet.center_y = start_y
+            # Position the bullet at the player's current location
+            start_x = self.player_sprite.center_x
+            start_y = self.player_sprite.center_y
+            bullet.center_x = start_x
+            bullet.center_y = start_y
 
-        # Get from the mouse the destination location for the bullet
-        # IMPORTANT! If you have a scrolling screen, you will also need
-        # to add in self.view_bottom and self.view_left.
-        dest_x = x + self.view_left
-        dest_y = y + self.view_bottom
+            # Get from the mouse the destination location for the bullet
+            # IMPORTANT! If you have a scrolling screen, you will also need
+            # to add in self.view_bottom and self.view_left.
+            dest_x = x + self.view_left
+            dest_y = y + self.view_bottom
 
-        # Do math to calculate how to get the bullet to the destination.
-        # Calculation the angle in radians between the start points
-        # and end points. This is the angle the bullet will travel.
-        x_diff = dest_x - start_x
-        y_diff = dest_y - start_y
-        angle = math.atan2(y_diff, x_diff)
+            # Do math to calculate how to get the bullet to the destination.
+            # Calculation the angle in radians between the start points
+            # and end points. This is the angle the bullet will travel.
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
 
-        # Angle the bullet sprite so it doesn't look like it is flying
-        # sideways.
-        bullet.angle = math.degrees(angle)
-        # DEBUG # print(f"Bullet angle: {bullet.angle:.2f}")
+            # Angle the bullet sprite so it doesn't look like it is flying
+            # sideways.
+            bullet.angle = math.degrees(angle)
+            # DEBUG # print(f"Bullet angle: {bullet.angle:.2f}")
 
-        # Taking into account the angle, calculate our change_x
-        # and change_y. Velocity is how fast the bullet travels.
-        bullet.change_x = math.cos(angle) * BULLET_SPEED
-        bullet.change_y = math.sin(angle) * BULLET_SPEED
+            # Taking into account the angle, calculate our change_x
+            # and change_y. Velocity is how fast the bullet travels.
+            bullet.change_x = math.cos(angle) * BULLET_SPEED
+            bullet.change_y = math.sin(angle) * BULLET_SPEED
 
-        # Play a bullet sound.
-        arcade.play_sound(self.bullet_sound)
+            # Play a bullet sound.
+            arcade.play_sound(self.bullet_sound)
 
-        # Add the bullet to the appropriate lists
-        self.bullet_list.append(bullet)
+            # Add the bullet to the appropriate lists
+            self.bullet_list.append(bullet)
 
     def on_update(self, delta_time):
         """ Movement and game logic """
